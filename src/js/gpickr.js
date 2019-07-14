302dd0902a5db8f57568aae08a3c7cf73ef17e31
Simon <toports@gmx.de>
2019-07-14 17:10:36 +0200

    Initial commit

---
+++ src/js/gpickr.js
@@ -0,0 +1,1 @@
+import '../scss/_main.scss';
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry:  './src/js/gpickr.js',
+
+    output: {
+        publicPath: 'dist',
+        filename: 'gpickr.min.js',
+        library: 'GPickr',
+        libraryExport: 'default',
+        libraryTarget: 'umd'
+    },
+
+    devServer: {
+        contentBase: `${__dirname}/`,
+        host: '0.0.0.0',
+        port: 3007
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new FixStyleOnlyEntriesPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'gpickr.min.css'
+        })
+    ]
+};
