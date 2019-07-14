7e2a95303476de2763c2cd2dec805ed74a7f54f9
Simon <toports@gmx.de>
2019-07-14 19:00:41 +0200

    Connect gradient engine with pickr

--- src/js/gpickr.js
+++ src/js/gpickr.js
@@ -4,11 +4,13 @@ import '@simonwep/pickr/src/scss/themes/nano.scss';
 import Pickr from '@simonwep/pickr';
 import dom   from './template';
 
-const {utils, libs} = Pickr;
+const {utils} = Pickr;
+const {on, off} = utils;
 
 class GPickr {
 
     _stops = [];
+    _focusedStop = null;
 
     constructor({el}) {
         el = typeof el === 'string' ? document.querySelector(el) : el;
@@ -19,6 +21,7 @@ class GPickr {
             theme: 'nano',
             inline: true,
             useAsButton: true,
+            defaultRepresentation: 'HEXA',
 
             components: {
                 palette: true,
@@ -27,22 +30,29 @@ class GPickr {
                 hue: true,
 
                 interaction: {
-                    hex: true,
-                    rgba: true,
-                    input: true,
-                    cancel: true
+                    input: true
                 }
             }
-        });
+        }).on('change', color => {
+            this._focusedStop.color = color.toRGBA().toString();
+            this._render();
+        }).on('init', () => {
+            on(dom.gradient.stops.preview, 'click', e => {
+                this._addStop(
+                    this._pickr.getColor().toRGBA().toString(),
+                    this._resolveColorStopPosition(e.pageX)
+                );
+            });
 
-        this._addStop('#ff846d', 0);
-        this._addStop('#ff88e6', 1);
-        this._render();
+            this._addStop('rgb(255,132,109)', 0);
+            this._addStop('rgb(255,136,230)', 1);
+        });
     }
 
     _render() {
         const {preview} = dom.gradient.stops;
         const {_stops} = this;
+        _stops.sort((a, b) => a.loc - b.loc);
 
         for (const {color, el, loc} of _stops) {
             Object.assign(el.style, {
@@ -61,13 +71,42 @@ class GPickr {
         const el = utils.createElementFromString('<div class="gpcr-marker"></div>');
         markers.appendChild(el);
 
-        const moveable = new libs.Moveable({
-            lock: 'v',
-            element: el,
-            wrapper: markers
+        const stop = {
+            el,
+            loc,
+            color
+        };
+
+        on(el, ['mousedown', 'touchstart'], () => {
+            this._pickr.setColor(stop.color);
+            this._focusedStop = stop;
+
+            const m = on(window, ['mousemove', 'touchmove'], e => {
+                stop.loc = this._resolveColorStopPosition(e.pageX);
+                this._render();
+            });
+
+            const s = on(window, ['mouseup', 'touchend', 'touchcancel'], () => {
+                off(...m);
+                off(...s);
+            });
         });
 
-        this._stops.push({el, moveable, loc, color});
+        this._focusedStop = stop;
+        this._pickr.setColor(stop.color);
+        this._stops.push(stop);
+        this._render();
+    }
+
+    _resolveColorStopPosition(x) {
+        const {markers} = dom.gradient.stops;
+        const diff = x - markers.offsetLeft;
+
+        let loc = diff / markers.offsetWidth;
+        if (loc < 0) loc = 0;
+        if (loc > 1) loc = 1;
+
+        return loc;
     }
 }
 
